71c261c707ccffadbb5e9cd5a685523f0385539f
ThierryJegen <s8thjege@stud.uni-saarland.de>
2020-01-21 19:01:36 +0100

    Added basic script layout

---
+++ Assets/Scripts/percentageCalculation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class percentageCalculation : MonoBehaviour
+{
+    enum State
+    {
+        Extrovert,
+        Introvert
+    } // Add more if necessary
+
+    private float scoreExtrovert { get; set; }
+    private float scoreIntrovert { get; set; }
+    public int maxScore { get; set; }
+    public int pointsPerLike { get; set; }
+    public float percentageExtrovert { get; set; }
+    public float percentageIntrovert { get; set; }
+
+    public State state;
+
+    private void setState()
+    {
+        if (percentageExtrovert > percentageIntrovert) state = State.Extrovert;
+        else state = State.Introvert;
+    }
+
+    private void calculateStuff() {
+        //TODO: Score calculations
+
+        percentageExtrovert = scoreExtrovert / maxScore;
+        percentageIntrovert = scoreIntrovert / maxScore;
+        setState();
+    }
+
+    public void showAd() { 
+        //TODO: show corresponding ad
+    }
+
+}
