b46df6b54633014de59b71c81abd1bfc0de780aa
ThierryJegen <s8thjege@stud.uni-saarland.de>
2020-01-27 15:18:10 +0100

    minor build issue fix

--- Assets/Scripts/DisplayResult.cs
+++ Assets/Scripts/DisplayResult.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.UIElements;
 
 public class DisplayResult : MonoBehaviour
 {
