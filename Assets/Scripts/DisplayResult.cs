642e8f3c0bd50dbff936648d6a6156bd2e594698
ThierryJegen <s8thjege@stud.uni-saarland.de>
2020-01-27 14:59:04 +0100

    Sollte jetzt alles drin sein muss aber noch auf einem Handy getestet werden

--- Assets/Scripts/DisplayResult.cs
+++ Assets/Scripts/DisplayResult.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.UIElements;
 
 public class DisplayResult : MonoBehaviour
 {
+    public GameObject Extroversion_Ad;
+    public GameObject Openess_Ad;
+
+    public Sprite image_Ad_E0;
+    public Sprite image_Ad_E1;
+    public Sprite image_Ad_O0;
+    public Sprite image_Ad_O1;
+
+
     private void OnEnable()
     {
         Hashtable results = Camera.main.GetComponent<User>().getResults();
@@ -12,5 +23,26 @@ public class DisplayResult : MonoBehaviour
 
         print("Extroversion: " + results[User.Categories.Extroversion]);
         print("Openess: " + results[User.Categories.Openess]);
+
+        if (results != null)
+        {
+            float extroversionValue = (float)results[User.Categories.Extroversion];
+            float openessValue = (float)results[User.Categories.Openess];
+
+            if (extroversionValue >= 0.5)
+                //Set Extroversion E1 Ad
+                Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E1;
+            else
+                //Set Extroversion E0 Ad
+                Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E0;
+
+            if (openessValue >= 0.5)
+                //Set Openess O1 Ad
+                Openess_Ad.GetComponent<Image>().sprite = image_Ad_O1;
+            else
+                //Set Extroversion O0 Ad
+                Openess_Ad.GetComponent<Image>().sprite = image_Ad_O0;
+            
+        }
     }
 }
