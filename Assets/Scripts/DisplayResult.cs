e2b3c3048d116cba99c7d1b8522afc8f9ed91f23
ThierryJegen <s8thjege@stud.uni-saarland.de>
2020-01-27 15:07:59 +0100

    Minor Bug fix

--- Assets/Scripts/DisplayResult.cs
+++ Assets/Scripts/DisplayResult.cs
@@ -19,30 +19,32 @@ public class DisplayResult : MonoBehaviour
     {
         Hashtable results = Camera.main.GetComponent<User>().getResults();
 
+        float extroversionValue = 0;
+        float openessValue = 0;
         //Show matching advertisements
 
         print("Extroversion: " + results[User.Categories.Extroversion]);
         print("Openess: " + results[User.Categories.Openess]);
 
-        if (results != null)
-        {
-            float extroversionValue = (float)results[User.Categories.Extroversion];
-            float openessValue = (float)results[User.Categories.Openess];
+        if(results[User.Categories.Extroversion] != null)
+            extroversionValue = (float)results[User.Categories.Extroversion];
 
-            if (extroversionValue >= 0.5)
-                //Set Extroversion E1 Ad
-                Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E1;
-            else
-                //Set Extroversion E0 Ad
-                Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E0;
+        if (results[User.Categories.Openess] != null)
+            openessValue = (float)results[User.Categories.Openess];
 
-            if (openessValue >= 0.5)
-                //Set Openess O1 Ad
-                Openess_Ad.GetComponent<Image>().sprite = image_Ad_O1;
-            else
-                //Set Extroversion O0 Ad
-                Openess_Ad.GetComponent<Image>().sprite = image_Ad_O0;
+        if (extroversionValue >= 0.5)
+            //Set Extroversion E1 Ad
+            Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E1;
+        else
+            //Set Extroversion E0 Ad
+            Extroversion_Ad.GetComponent<Image>().sprite = image_Ad_E0;
+
+        if (openessValue >= 0.5)
+            //Set Openess O1 Ad
+            Openess_Ad.GetComponent<Image>().sprite = image_Ad_O1;
+        else
+            //Set Extroversion O0 Ad
+            Openess_Ad.GetComponent<Image>().sprite = image_Ad_O0;
             
-        }
     }
 }
